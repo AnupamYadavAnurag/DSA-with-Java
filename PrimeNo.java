de1a062b0025f79eaa0c708c300af1c659ce0311
anupamyadavanurag <anuragyadavanupam@gmail.com>
2025-10-23 10:53:08 +0530

    prime

--- PrimeNo.java
+++ PrimeNo.java
@@ -1,10 +1,34 @@
-public class PrimeNo {
-    dhgff 
+import java.util.Scanner;
 
-    g
-    rgwre
-    wergw
-    erewg
-    erewgwew
-    wrgw 
+public class PrimeNo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+
+        // 0 and 1 are not prime
+        if (num <= 1) {
+            System.out.println(num + " is not a prime number.");
+            return;
+        }
+
+        boolean isPrime = true;
+
+        // Check divisibility from 2 to sqrt(num)
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                isPrime = false;
+                break; // no need to check further
+            }
+        }
+
+        if (isPrime) {
+            System.out.println(num + " is a prime number.");
+        } else {
+            System.out.println(num + " is not a prime number.");
+        }
+
+        sc.close();
+    }
 }
